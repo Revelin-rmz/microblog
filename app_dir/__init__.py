85402593c1cf404c3fa207a58ee75478d40e2973
Ahmad Raamiz <raamiz216@gmail.com>
2023-05-30 21:18:16 -0400

    Craeted schema for DB and initialized

--- app_dir/__init__.py
+++ app_dir/__init__.py
@@ -1,7 +1,19 @@
 from flask import Flask
-from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+"""
+keep this import below cause fucking hell the flask db init wont work otherwise,
+see jwmuray @ https://github.com/miguelgrinberg/microblog/issues/69
+ignore the tutorial
+"""
+from config import Config           # nopep8
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
-from app_dir import routes  # nopep8
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app_dir import routes, models  # nopep8
